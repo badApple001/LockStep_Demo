721304022968b375a2bf337f6d1f358505f0a089
极客柒 <58359338+badApple001@users.noreply.github.com>
2025-07-26 18:03:02 +0800

    优化

--- Assets/Scripts/System/AEDebug.cs
+++ Assets/Scripts/System/AEDebug.cs
@@ -1,13 +1,17 @@
 #define DEBUGMODE
+using System.Diagnostics;
+
 namespace LockStep_Demo
 {
     public static class AEDebug
     {
+        [Conditional( "DEBUGMODE" )]
         public static void Log(object msg)
         {
             Log(msg.ToString());
         }
 
+        [Conditional( "DEBUGMODE" )]
         public static void Log(string msg)
         {
 #if DEBUGMODE
